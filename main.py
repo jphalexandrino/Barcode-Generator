8ea55588b6547b72fb9429ad8f7a7a8abbc38ff9
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2024-05-13 16:37:57 -0300

    add a new method to `send a product`

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+import csv
 from barcode import EAN13
 from barcode.writer import ImageWriter
 
-name = input("Coloque o nome do produto: ")
-number = input("Coloque o numero do produto: ")
-if  len(number) != 12 or number[0]!="97" :
-
-    my_code = EAN13(number, writer=ImageWriter())
-    my_code.save('barcodes/barcode_{}'.format(name))
+def create_barcode(name, number):
     
-    print("Código de barras criado! ")
-else :
-    print ("É necessário que o produto tenha 12 digitos!")
+    if len(number) != 12 or number[0] != "97":
+        
+        my_code = EAN13(number, writer=ImageWriter())
+        my_code.save('barcodes/barcode_{}'.format(name))
+        
+        print("Código de barras criado para {}!".format(name))
+    else:
+        print("É necessário que o produto tenha 12 dígitos!")
+
+def read_csv(filename):
+    
+    with open(filename, 'r', newline='') as file:
+        
+        reader = csv.reader(file)
+        next(reader) 
+        
+        for row in reader:
+            name, number = row
+            create_barcode(name, number)
+
+# Name of the CSV
+csv_filename = 'products.csv'
+read_csv(csv_filename)
